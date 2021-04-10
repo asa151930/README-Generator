438defe7450cafa27c8287d36be624d3f2edb799
Ali Arab <aliarab812@yahoo.com>
2021-04-10 00:24:07 -0500

    adding prompts for questions

--- index.js
+++ index.js
@@ -8,11 +8,46 @@ inquirer
     .prompt([
         {
             type: 'input',
-            message: 'What is the title of your Project?',
-            name: '',
-            
+            name: 'title',
+            message: 'Enter the title of the project: ',
         },
+
         {
-            
+            type: 'input',
+            name: 'description',
+            message: 'What does the project do? Enter a brief description: ', 
+        }, 
+
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'In a few words, describe the installation process: ',
+        },
+
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is the project used for: ',
+        },
+
+        {
+            type: 'input',
+            name: 'contributor',
+            message: 'Name the contributors if any: ',
+        },
+
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Any test instructions available? ',
+        },
+
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Enter the type of licenses used using the given options: ',
+            choices: [
+                
+            ]
         }
     ]);
