aae02ac3474da40ffa218fa284bdd5a1591521b8
Ali Arab <aliarab812@yahoo.com>
2021-04-10 00:34:05 -0500

    updates on prompts

--- index.js
+++ index.js
@@ -1,48 +1,40 @@
-
-// add inquirer to ask questions 
+// added inquirer to ask questions 
 const inquirer = require('inquirer');
 
 const fs = require('fs');
 const readme_style = require('./readme_style');
 
-inquirer
-    .prompt([
+inquirer.prompt([
         {
             type: 'input',
             name: 'title',
             message: 'Enter the title of the project: ',
         },
-
         {
             type: 'input',
             name: 'description',
             message: 'What does the project do? Enter a brief description: ', 
         }, 
-
         {
             type: 'input',
             name: 'installation',
             message: 'In a few words, describe the installation process: ',
         },
-
         {
             type: 'input',
             name: 'usage',
             message: 'What is the project used for: ',
         },
-
         {
             type: 'input',
             name: 'contributor',
             message: 'Name the contributors if any: ',
         },
-
         {
             type: 'input',
             name: 'test',
             message: 'Any test instructions available? ',
         },
-
         {
             type: 'list',
             name: 'license',
@@ -54,17 +46,14 @@ inquirer
               GNU,
               IBM,
               MIT, 
-              Mozilla,
               ISC,
             ]
         },
-
         {
             type: 'input',
             name: 'username',
             message: 'Enter your Git-Hub Username: ',
         },
-
         {
             type: 'input',
             name: 'email',
