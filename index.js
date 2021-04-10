8331d5e018bf46ffa67da776fff988455cb7dc84
Ali Arab <aliarab812@yahoo.com>
2021-04-09 23:59:51 -0500

    added inquirer for node modules

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 
-
-// create node modules
-
 // add inquirer to ask questions 
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const readme_style = require("./readme_style");
 
 inquirer
     .prompt([
         {
-            type: '',
+            type: 'input',
             message: 'What is the title of your Project?',
             name: '',
             
--- readme_style.js
+++
@@ -1,4 +0,0 @@
-const x = function () {
-    console.log("Just adding this file to modules");
-}
-
