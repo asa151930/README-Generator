b2d3f5ef23cd73bee5d64e620ed9b4bc72e3d024
Ali Arab <aliarab812@yahoo.com>
2021-04-10 21:36:25 -0500

    adding a test example of the readme

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ inquirer.prompt([
 When creating an open source project on GitHub, it is important to have a quality README with information about the app--what is the app for, how to use the app, how to install it, how to report issues, and how to make contributions so that other developers are more likely to use and contribute to the success of the project. A command-line application will allow for quick and easy generation of a project README to get started quickly.
  This will allow a project creator to spend more time working on finishing the project and less time creating a good README.
 <p>${description}</p>
-<br></br>
+<br>
 
 <h2>Table of Contents</h2>
   <ul> 
@@ -83,33 +83,33 @@ When creating an open source project on GitHub, it is important to have a qualit
    <li><a href="#Contributor">Contributor</a></li>   
    <li><a href="#Tests">Tests</a></li>   
    <li><a href="#Questions">Questions</a></li>                         
-  </ul>
+  </ul> <br>
 
   <h2 id="Installation">Installation</h2>  
-  GIVEN a command-line application that accepts user input 
-  WHEN I am prompted for information about my application repository, 
-  THEN a quality, professional README.md is generated with the title of your project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions. 
-  WHEN I enter my project title, THEN this is displayed as the title of the README. 
-  WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions, 
-  THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests. 
-  WHEN I choose a license for my application from a list of options, THEN a badge for that license is added hear the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under. 
-  WHEN I enter my GitHub username, THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile. 
-  WHEN I enter my email address, THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions. 
-  WHEN I click on the links in the Table of Contents, THEN I am taken to the corresponding section of the README.       
-
+  To generate your own README, first run npm install in order to install the following npm package dependencies as specified in the package.json:
+  - inquirer that will prompt you for your inputs from the command line
+  - The application itself can be invoked with node index.js.     
   <p>${installation}</p>
+ 
   <h2 id="Usage">Usage</h2>
-  <p>${usage}</p> 
+  <p> When you run node index.js, the application uses the inquirer package to prompt you in the command line with a series of questions about your GitHub and about your project. </p>
+  <p>${usage}</p>
+
   <h2 id="License">License</h2>
   <p>The license used for the project is <a href="./README.md">${license}</a> license.</p>
+  
   <h2 id="Contributor">Contributor</h2>
+  <p> Feel free to fork. </p>
   <p>${contributor}</p>
+
   <h2 id="Tests">Tests</h2>
   <p>${test}</p>
+  
   <h2 id="Questions">Questions</h2> 
   <p>If you have any questions, please reach out to the information provided below. </p> <br>
+  
   GitHub: @${username} <br>
-  Email: ${email}`; 
+  Email: ${email}`;
 
         newFile(title, readme_style);
 
